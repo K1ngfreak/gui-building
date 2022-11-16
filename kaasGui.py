10d44611a8cdfd1c5aba55afaaf90313dda3b9dc
K1ngfreak <noahzonneveld@hotmail.com>
2022-11-16 15:41:04 +0100

    begin is klaar, nu alleen nog groter maken

--- kaasGui.py
+++ kaasGui.py
@@ -1,51 +1,72 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.messagebox import showinfo
 
 window = tk.Tk()
 window.title('Kaas')
+window.geometry('300x120')
 
 kaas = ('Emmenthalen', 'Leerdammer', 'Pammigiano Reggiano', 'Goudse kaas', 'Blue de Rochbaron', 'Foume d_Ambert', 'Camembert', 'Mozzarella')
 
+def option1_1():
+    pb['value'] += 33.5
+    window.geometry('350x120')
+    option_button1.configure(text='Er zitten gaten in de kaas', command=option1_2)
+    option_button2.configure(text='Er zitten geen gaten in de kaas', command=option1_4)
 
+def option1_2():
+    pb['value'] += 33.5
+    window.geometry('300x120')
+    option_button1.configure(text='De prijs is erg hoog', command=option1_3)
+    option_button2.configure(text='De prijs valt mee')
 
+def option1_3():
+    pb['value'] += 33
+    showinfo(message='Je hebt: Emmenthaler')
 
+def option1_4():
+    pb['value'] += 33.5
+    option_button1.configure(text='',command=option1_5)
 
+def option2_1():
+    pb['value'] += 33.5
+    window.geometry('430x120')
+    option_button1.configure(text='De kaas heeft blauwe schimmels')
+    option_button2.configure(text='De kaas heeft geen blauwe schimmels')
 
+def option2_2():
+    pb['value'] += 33.5
+    option_button1.configure()
+    option_button2.configure()
 
+def option2_3():
+    pb['value'] += 33
+    option_button1.configure()
+    option_button2.configure()
+
+pb = ttk.Progressbar(
+    window,
+    orient='horizontal',
+    mode='determinate',
+    length=280
+)
+
+option_button1 = ttk.Button(
+    window,
+    text='de kaas is geel',
+    command=option1_1
+)
+
+option_button2 = ttk.Button(
+    window,
+    text='de kaas is niet geel',
+    command=option2_1
+)
+
+pb.grid(column=0, row=0, columnspan=2, padx=10, pady=20)
+
+option_button1.grid(column=0, row=2, padx=10, pady=10, sticky=tk.E)
+
+option_button2.grid(column=1, row=2, padx=10, pady=10, sticky=tk.W)
 
 window.mainloop()
-
-
-
-
-
-
-kaas = str(input('Is de kaas geel? '))
-if kaas == 'ja':
-    gaten = str(input('Zitter er gaten in? '))
-    if gaten == 'ja':
-        duur = str(input('Is de kaas belachelijk duur? '))
-        if duur == 'ja':
-            print('Emmenthaler')
-        else:
-            print('Leerdammer')
-    else:
-        steen = str(input('Is de kaas hard als steen? '))
-        if steen == 'ja':
-            print('Pammigiano Reggiano')
-        else:
-            print('Goudse kaas')
-else:
-    schimmels = str(input('Heeft de kaas blauwe schimmels? '))
-    if schimmels == 'ja':
-        korst1 = str(input('Heeft de kaas een korst? '))
-        if korst1 == 'ja':
-            print('Bleu de Rochbaron')
-        else:
-            print('Foume d_Ambert')
-    else:
-        kosrt2 = str(input('Heeft de kaas een korst? '))
-        if kosrt2 == 'ja':
-            print('Camembert')
-        else:
-            print('mozzarella')
