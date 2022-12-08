d00cadf03a747040d4d9769da5749c4f29e43847
K1ngfreak <noahzonneveld@hotmail.com>
2022-12-08 13:40:40 +0100

    alle vragen die er vaker in staan eruit gehaald

--- solicitatieGui.py
+++ solicitatieGui.py
@@ -14,6 +14,8 @@ def next_question():
         question_label.config(text='Hoeveel jaar ervaring heeft u met acrobatiek?') # 3 jaar
     elif question == 3:
         question_label.config(text='Welk niveau MBO diploma heeft u?') # niveau 4
+    elif question == 4:
+        question_label.config(text='Heeft u een geldig vrachtwagen rijbewijs?') # ja
 
 
 window = tk.Tk()
@@ -64,10 +66,7 @@ window.mainloop()
 
 
 
-    MBO = input('Wat voor MBO diploma heeft u? ')
-    if MBO >= '4':
-        rijbewijs = str(input('Heeft u een geldig vrachtwagen rijbewijs? '))
-        if rijbewijs == 'ja':
+
             hoed = str(input('Heeft u een hoge hoed? '))
             if hoed == 'ja':
                 Geslacht = str(input('Bent u een man of vrouw? '))
@@ -104,144 +103,4 @@ window.mainloop()
                                 if gewicht_persoon >= '90':
                                     certificaat = str(input('Heeft u het certificaat: “Overleven met gevaarlijk personeel”? '))
                                     if certificaat == 'ja':
-                                        print('U kan op solicitatie')
-                                    else:
-                                        print('U kwalificeert niet voor de baan')
-                                else:
-                                    print('U kwalificeert niet voor de baan')
-                            else:
-                                print('U kwalificeert niet voor de baan')
-                        else:
-                            print('U kwalificeert niet voor de baan')
-                    else:
-                        print('U kwalificeert niet voor de baan')
-                else:
-                    print('U kwalificeert niet voor de baan')
-            else:
-                print('U kwalificeert niet voor de baan')
-        else:
-            print('U kwalificeert niet voor de baan')
-    else:
-        print('U kwalificeert niet voor de baan')
 
-        MBO = input('Wat voor MBO diploma heeft u? ')
-        if MBO == '4':
-            rijbewijs = str(input('Heeft u een geldig vrachtwagen rijbewijs? '))
-            if rijbewijs == 'ja':
-                hoed = str(input('Heeft u een hoge hoed? '))
-                if hoed == 'ja':
-                    Geslacht = str(input('Bent u een man of vrouw? '))
-                    if Geslacht == 'man':
-                        snor = str(input('Heeft u een snor? '))
-                        if snor == 'ja':
-                            lengte_snor = input('Hoeveel cm is uw snor? ')
-                            if lengte_snor >= '10':
-                                lengte_person = input('Hoeveel cm bent u? ')
-                                if lengte_person >= '150':
-                                    gewicht_persoon = input('Hoeveel weegt u in kg? ')
-                                    if gewicht_persoon >= '90':
-                                        certificaat = str(input('Heeft u het certificaat: “Overleven met gevaarlijk personeel”? '))
-                                        if certificaat == 'ja':
-                                            print('U kan op solicitatie')
-                                        else:
-                                            print('U kwalificeert niet voor de baan')
-                                    else:
-                                        print('U kwalificeert niet voor de baan')
-                                else:
-                                    print('U kwalificeert niet voor de baan')
-                            else:
-                                print('U kwalificeert niet voor de baan')
-                        else:
-                            print('U kwalificeert niet voor de baan')
-                    elif Geslacht == 'vrouw':
-                        krulhaar = str(input('Draagt u rood krulhaar? '))
-                        if krulhaar == 'ja':
-                            lengte_krulhaar = input('Hoelang is uw haar? ')
-                            if lengte_krulhaar > '20':
-                                lengte_person = input('Hoeveel cm bent u? ')
-                                if lengte_person >= '150':
-                                    gewicht_persoon = input('Hoeveel weegt u in kg? ')
-                                    if gewicht_persoon >= '90':
-                                        certificaat = str(input('Heeft u het certificaat: “Overleven met gevaarlijk personeel”? '))
-                                        if certificaat == 'ja':
-                                            print('U kan op solicitatie')
-                                        else:
-                                            print('U kwalificeert niet voor de baan')
-                                    else:
-                                        print('U kwalificeert niet voor de baan')
-                                else:
-                                    print('U kwalificeert niet voor de baan')
-                            else:
-                                print('U kwalificeert niet voor de baan')
-                        else:
-                            print('U kwalificeert niet voor de baan')
-                    else:
-                        print('U kwalificeert niet voor de baan')
-                else:
-                    print('U kwalificeert niet voor de baan')
-            else:
-                print('U kwalificeert niet voor de baan')
-        else:
-            print('U kwalificeert niet voor de baan')
-
-            MBO = input('Wat voor MBO diploma heeft u? ')
-            if MBO == '4':
-                rijbewijs = str(input('Heeft u een geldig vrachtwagen rijbewijs? '))
-                if rijbewijs == 'ja':
-                    hoed = str(input('Heeft u een hoge hoed? '))
-                    if hoed == 'ja':
-                        Geslacht = str(input('Bent u een man of vrouw? '))
-                        if Geslacht == 'man':
-                            snor = str(input('Heeft u een snor? '))
-                            if snor == 'ja':
-                                lengte_snor = input('Hoeveel cm is uw snor? ')
-                                if lengte_snor >= '10':
-                                    lengte_person = input('Hoeveel cm bent u? ')
-                                    if lengte_person >= '150':
-                                        gewicht_persoon = input('Hoeveel weegt u in kg? ')
-                                        if gewicht_persoon >= '90':
-                                            certificaat = str(input('Heeft u het certificaat: “Overleven met gevaarlijk personeel”? '))
-                                            if certificaat == 'ja':
-                                                print('U kan op solicitatie')
-                                            else:
-                                                print('U kwalificeert niet voor de baan')
-                                        else:
-                                            print('U kwalificeert niet voor de baan')
-                                    else:
-                                        print('U kwalificeert niet voor de baan')
-                                else:
-                                    print('U kwalificeert niet voor de baan')
-                            else:
-                                print('U kwalificeert niet voor de baan')
-                        elif Geslacht == 'vrouw':
-                            krulhaar = str(input('Draagt u rood krulhaar? '))
-                            if krulhaar == 'ja':
-                                lengte_krulhaar = input('Hoelang is uw haar? ')
-                                if lengte_krulhaar > '20':
-                                    lengte_person = input('Hoeveel cm bent u? ')
-                                    if lengte_person >= '150':
-                                        gewicht_persoon = input('Hoeveel weegt u in kg? ')
-                                        if gewicht_persoon >= '90':
-                                            certificaat = str(input('Heeft u het certificaat: “Overleven met gevaarlijk personeel”? '))
-                                            if certificaat == 'ja':
-                                                print('U kan op solicitatie')
-                                            else:
-                                                print('U kwalificeert niet voor de baan')
-                                        else:
-                                            print('U kwalificeert niet voor de baan')
-                                    else:
-                                        print('U kwalificeert niet voor de baan')
-                                else:
-                                    print('U kwalificeert niet voor de baan')
-                            else:
-                                print('U kwalificeert niet voor de baan')
-                        else:
-                            print('U kwalificeert niet voor de baan')
-                    else:
-                        print('U kwalificeert niet voor de baan')
-                else:
-                    print('U kwalificeert niet voor de baan')
-            else:
-                print('U kwalificeert niet voor de baan')
-        else:
-            print('U kwalificeert niet voor de baan')
